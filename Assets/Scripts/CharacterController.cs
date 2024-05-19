137223ab842e5210bf4f793b4887182128363d7e
Hatice Akgül <1akgullhaticee@gmail.com>
2024-05-20 00:10:51 +0300

    yeni sahneler ve scriptler
    karakter hareketi ,  normal scene , deprem scene ve puzzlelar

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 3f; // Karakterin hareket hýzý
+    public float jumpForce = 3f; // Karakterin zýplama kuvveti
+    bool turnRight = true;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    Animator playerAnimator;
+    Vector3 scale;
+    float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+            playerAnimator.SetBool("Jump", true);
+        }
+        if(Mathf.Approximately(rb.velocity.y, 0)&&playerAnimator.GetBool("Jump")) {
+            playerAnimator.SetBool("Jump", false);
+        }
+        if (moveInput > 0 && turnRight == false)
+        {
+            flip();
+           
+        }
+        if (moveInput < 0 && turnRight == true)
+        {
+            flip();
+            
+        }
+
+    }
+
+    void Move()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        playerAnimator.SetFloat("Speed" , Mathf.Abs(moveInput));
+        
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Eðer karakter zeminle temas ederse, isGrounded'ý true yap
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // Eðer karakter zeminden ayrýlýrsa, isGrounded'ý false yap
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    void flip()
+    {
+        //turnRight = !turnRight;
+        scale= gameObject.transform.localScale;
+        scale.x *= -1;
+        gameObject.transform.localScale = scale;
+        
+    }
+}
--- Assets/Scripts/GameManagerForPicturePuzzle.cs
+++ Assets/Scripts/GameManagerForPicturePuzzle.cs
@@ -1,21 +1,175 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using TMPro;
 
 public class GameManagerForPicturePuzzle : MonoBehaviour
 {
     [SerializeField] private Transform gameTransform;
     [SerializeField] private Transform piecePrefabs;
 
+    private List<Transform> pieces;
     private int emptyLocation;
     private int size;
+    private bool shuffling = false;
+    public float countdownTime = 20f; // Geri sayýmýn süresi
+    public TMP_Text countdownText;
+    private float currentTime;
+    private bool gameStarted = false; // Oyunun baþladýðýný kontrol etmek için flag
 
     private void CreateGamePieces(float gapThickness)
     {
-        float width = 1 /  (float)size;
+        float width = 1 / (float)size;
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                Transform piece = Instantiate(piecePrefabs, gameTransform);
+                pieces.Add(piece);
 
+                piece.localPosition = new Vector3(-1 + (2 * width * col) + width, +1 - (2 * width * row) - width, 0);
 
+                piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
+                piece.name = $"{(row * size) + col}";
+
+                if ((row == size - 1) && (col == size - 1))
+                {
+                    emptyLocation = (size * size) - 1;
+                    piece.gameObject.SetActive(false);
+                }
+                else
+                {
+                    float gap = gapThickness / 2;
+                    Mesh mesh = piece.GetComponent<MeshFilter>().mesh;
+                    Vector2[] uv = new Vector2[4];
+                    uv[0] = new Vector2((width * col) + gap, 1 - ((width * (row + 1)) - gap));
+                    uv[1] = new Vector2((width * (col + 1)) - gap, 1 - ((width * (row + 1)) - gap));
+                    uv[2] = new Vector2((width * col) + gap, 1 - ((width * row) + gap));
+                    uv[3] = new Vector2((width * (col + 1)) - gap, 1 - ((width * row) + gap));
+
+                    mesh.uv = uv;
+                }
+            }
+        }
     }
 
+    private void Start()
+    {
+        currentTime = countdownTime;
+        pieces = new List<Transform>();
+        size = 3;
+        CreateGamePieces(0.01f);
+        StartCoroutine(WaitShuffle(0.5f)); // Puzzle'ý oluþturduktan sonra karýþtýrma iþlemini baþlat
+    }
 
+    private void Update()
+    {
+        if (!gameStarted) return; // Oyun baþlamadýysa güncelleme yapma
+
+        currentTime -= Time.deltaTime;
+
+        if (countdownText != null)
+        {
+            countdownText.text = currentTime.ToString("0");
+        }
+
+        if (currentTime <= 0)
+        {
+            if (CheckCompletion()) // Kazanma
+            {
+                SceneManager.LoadScene(2); // Kazanma ekraný sahnesini yükle
+            }
+            else // Kaybetme
+            {
+                SceneManager.LoadScene(3); // Kaybetme ekraný sahnesini yükle
+            }
+        }
+        else if (CheckCompletion())
+        {
+            SceneManager.LoadScene(2); // Kazanma ekraný sahnesini yükle
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    if (pieces[i] == hit.transform)
+                    {
+                        if (SwapIfValid(i, -size, size)) { break; }
+                        if (SwapIfValid(i, +size, size)) { break; }
+                        if (SwapIfValid(i, -1, 0)) { break; }
+                        if (SwapIfValid(i, +1, size - 1)) { break; }
+                    }
+                }
+            }
+        }
+    }
+
+    private bool SwapIfValid(int i, int offset, int colCheck)
+    {
+        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        {
+            (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+            (pieces[i].localPosition, pieces[i + offset].localPosition) = (pieces[i + offset].localPosition, pieces[i].localPosition);
+            emptyLocation = i;
+            return true;
+        }
+        return false;
+    }
+
+    private bool CheckCompletion()
+    {
+        for (int i = 0; i < pieces.Count; ++i)
+        {
+            if (pieces[i].name != $"{i}")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerator WaitShuffle(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Shuffle();
+        shuffling = false;
+        gameStarted = true; // Karýþtýrma tamamlandýktan sonra oyunu baþlat
+    }
+
+    private void Shuffle()
+    {
+        int count = 0;
+        int last = emptyLocation;
+        while (count < (size * size * size))
+        {
+            int rnd = Random.Range(0, size * size);
+            if (rnd == last)
+            {
+                continue;
+            }
+            last = rnd;
+
+            if (SwapIfValid(rnd, -size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +size, size))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, -1, 0))
+            {
+                count++;
+            }
+            else if (SwapIfValid(rnd, +1, size - 1))
+            {
+                count++;
+            }
+        }
+    }
 }
